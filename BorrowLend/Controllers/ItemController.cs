b57a8f21be7c1b7e3090ccef97806613014c5ce6
dan4onarkoman40 <dd93578373@edu.mon.bg>
2024-11-08 11:15:23 +0200

    first commit

---
+++ BorrowLend/Controllers/ItemController.cs
@@ -0,0 +1,109 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using BorrowLend.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BorrowLend.Data;
+
+namespace BorrowLend.Controllers
+{
+    public class ItemController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public ItemController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        // GET: Item
+        public IActionResult Index()
+        {
+            IEnumerable<Item> obj = _db.Items;
+            return View(obj);
+        }
+
+        // GET: Item/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Item/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Item item)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Items.Add(item);
+                await _db.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(item);
+        }
+
+        // GET: Item/Edit/5
+        public async Task<IActionResult> Update(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var item = await _db.Items.FindAsync(id);
+            if (item == null)
+            {
+                return NotFound();
+            }
+            return View(item);
+        }
+
+        // POST: Item/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Update(int id, Item item)
+        {
+            if (id != item.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                _db.Update(item);
+                await _db.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(item);
+        }
+
+        // GET: Item/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var item = await _db.Items.FindAsync(id);
+            if (item == null)
+            {
+                return NotFound();
+            }
+
+            return View(item);
+        }
+
+        // POST: Item/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var item = await _db.Items.FindAsync(id);
+            _db.Items.Remove(item);
+            await _db.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
---
+++ BorrowLend/Data/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using BorrowLend.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BorrowLend.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+        public DbSet<Item> Items { get; set; }
+    }
+}
